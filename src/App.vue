1188ef13d13ffd92a9d6b8b441a3407399f8ee07
ryota.nakayama <ryota.nakayama@optim.co.jp>
2020-07-26 17:05:21 +0900

    microCMSからAPI取得

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Article></Article>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Article from './pages/Article.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Article
   }
 }
 </script>
---
+++ src/pages/Article.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ info }}</h1>
+    <h1>OK</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      info: "",
+    };
+  },
+  mounted() {
+    axios
+      .get("https://muscletech.microcms.io/api/v1/blog/k6zimc9au", {
+        headers: { "X-API-kEY": "5f3d06aa-83c1-47d2-845a-419a9d4335ed" },
+      })
+      .then((response) => (this.info = response.data.title));
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
